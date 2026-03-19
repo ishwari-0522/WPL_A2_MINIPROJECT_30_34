5f41fac6f95b787aa4ca1d780452a4b50d1b9f6a
Asavari Charati <asavaricharati@gmail.com>
2026-03-19 22:01:30 +0530

    added user authentication-register and login

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config();
 const db = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/',(req,res) => {
 res.json({ message: 'Microfinance app running!!' });
