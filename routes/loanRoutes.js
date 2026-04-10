37376d5254301c9ffc84049b2e037445c682c175
ishwari-0522 <ishwarichopade18@gmail.com>
2026-04-10 16:27:26 +0530

    backend change

--- routes/loanRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { applyLoan, getMyLoans } = require('../controllers/loanController');
-const protect = require('../middleware/authMiddleware');
-
-router.post('/apply', protect, applyLoan);
-router.get('/my-loans', protect, getMyLoans);
-
-module.exports = router;
--- routes/repaymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { makeRepayment, getRepayments } = require('../controllers/repaymentController');
-const protect = require('../middleware/authMiddleware');
-
-router.post('/pay', protect, makeRepayment);
-router.get('/:loan_id', protect, getRepayments);
-
-module.exports = router;
